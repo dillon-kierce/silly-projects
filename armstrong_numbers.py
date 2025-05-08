97397b184d202479f2ab92c63f315624ceae2145
= <=>
2025-05-07 23:52:41 -0400

    start of project

---
+++ armstrong_numbers.py
@@ -0,0 +1,8 @@
+"""
+Take a number and determine whether a number is an Armstrong number
+"""
+
+number = int(input("Please enter a valid number: "))
+
+def is_armstrong_number(number):
+    length = len(str(number))
