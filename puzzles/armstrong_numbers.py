5c06619c69940e12a009fb0a177529ef7f265c52
dillon-kierce <dillon.kierce@proton.me>
2025-05-13 09:28:37 -0400

    Update armstrong_numbers.py

--- puzzles/armstrong_numbers.py
+++ puzzles/armstrong_numbers.py
@@ -1,8 +1,18 @@
 """
-Take a number and determine whether a number is an Armstrong number
+This is an armstrong number verifier
 """
+input = int(input("Enter a valid number: "))
 
-number = int(input("Please enter a valid number: "))
+def is_armstrong_number(input):
+    """
+    Takes a value, splits into a list, and takes the power of length across all digits to dictate if its armstrong number
+    """
+    length = len(str(input))
+    numbers = [int(x) for x in str(input)]
+    sum_raised = sum([x ** int(length) for x in numbers])
+    if sum_raised == input:
+        print("You have found a armstrong number! ")
+    else:
+        print("Invalid number try again")
 
-def is_armstrong_number(number):
-    length = len(str(number))
+is_armstrong_number(input)
